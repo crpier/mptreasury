f363e2a3e1bfdbc1513975f5973257da13870969
crpier <crpier42@gmail.com>
2022-04-25 08:47:03 +0300

    refactor model

--- app/model.py
+++ app/model.py
@@ -1,56 +1,52 @@
-import datetime
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 
 class RawSong:
-    def __init__(self, title: str, album_name: str, artist_name: Optional[str] = None):
-        self.title = title
-        self.album_name = album_name
-        self.artist_name = artist_name
-
-
-class Song:
     def __init__(
-        self,
-        id: int,
-        title: str,
-        # genre: str,
-        # released: datetime.datetime,
-        path: Path,
-        # album_id: int,
-        album_name: str,
-        # artist_id: int,
-        artist_name: str,
-        provider_id: Optional[int] = None,
+        self, title: str, album_name: str, path: Path, artist_name: Optional[str] = None
     ):
-        self.id = id
         self.title = title
-        # self.genre = genre
-        # self.released = released
-        """TODO: custom type that ensures
-        - path is absolute
-        - optionally, has a prefix"""
-        self.path = path
-        # self.album_id = album_id
         self.album_name = album_name
-        # self.artist_id = artist_id
         self.artist_name = artist_name
-        self.provider_id = provider_id
+        self.path = path
+
+
+class RawAlbum:
+    def __init__(self, songs: List[RawSong]):
+        self.name = songs[0].album_name
+        self.artist_name = songs[0].artist_name
+        self.track_names: List[str] = []
+        self.songs: List[RawSong] = []
+        for song in songs:
+            self._validate_song(song)
+            self.track_names.append(song.title)
+            self.songs.append(song)
+
+    def _validate_song(self, song: RawSong):
+        if song.artist_name != self.artist_name:
+            raise ValueError("song does not have same artist as album")
+        if song.album_name != self.name:
+            raise ValueError("song does not have same album_name as album")
+
+    def song_by_title(self, title: str):
+        for song in self.songs:
+            if song.title == title:
+                return song
 
 
 class Album:
     def __init__(
         self,
-        id: int,
         name: str,
         genre: str,
-        released: datetime.datetime,
+        released: int,
         artist_id: int,
         artist_name: str,
-        master_provider_id: Optional[int] = None,
-        master_name: Optional[str] = None,
-        provider_id: Optional[int] = None,
+        master_provider_id: int,
+        master_name: str,
+        provider_id: int,
+        id: Optional[int] = None,
     ):
         self.id = id
         self.name = name
@@ -68,6 +64,35 @@ class Artist:
         self.id = id
         self.name = name
         self.provider_id = provider_id
+
+
+class Song:
+    def __init__(
+        self,
+        title: str,
+        # genre: str,
+        # released: datetime.datetime,
+        path: Path,
+        # album_id: int,
+        album_name: str,
+        # artist_id: int,
+        artist_name: str,
+        album: Album,
+        id: Optional[int] = None,
+    ):
+        self.id = id
+        self.title = title
+        # self.genre = genre
+        # self.released = released
+        """TODO: custom type that ensures
+        - path is absolute
+        - optionally, has a prefix"""
+        self.path = path
+        # self.album_id = album_id
+        self.album_name = album_name
+        # self.artist_id = artist_id
+        self.artist_name = artist_name
+        self.album = album
 
 
 UnknownArtist = Artist(id=-1, name="Unkown Artist")
--- app/test_service.py
+++ app/test_service.py
@@ -52,11 +52,10 @@ def raw_artist_with_one_album_with_one_song() -> service.OsWalkResult:
 @mark.unit
 def test_gather_album_with_one_song(raw_album_with_one_song: service.OsWalkResult):
     songs = service.gather_songs(raw_album_with_one_song)
-    assert songs._songs[0].artist is UnknownArtist
-    assert songs._songs[0].album.name == "test album"
-    assert songs._songs[0].title == "First song"
-    assert len(songs._songs) == 1
-    assert not songs.is_empty()
+    assert songs[0].artist_name == UnknownArtist.name
+    assert songs[0].album_name == "test album"
+    assert songs[0].title == "First song"
+    assert len(songs) == 1
 
 
 @mark.unit
@@ -66,11 +65,10 @@ def test_gather_album_with_artist_in_dir_name_with_one_song(
     songs = service.gather_songs(
         raw_artist_with_one_album_with_artist_in_dir_name_with_one_song
     )
-    assert songs._songs[0].artist.name == "test artist"
-    assert songs._songs[0].album.name == "test album"
-    assert songs._songs[0].title == "First song"
-    assert len(songs._songs) == 1
-    assert not songs.is_empty()
+    assert songs[0].artist_name == "test artist"
+    assert songs[0].album_name == "test album"
+    assert songs[0].title == "First song"
+    assert len(songs) == 1
 
 
 @mark.unit
@@ -79,8 +77,7 @@ def test_gather_artist_with_one_album(
     raw_artist_with_one_album_with_one_song: service.OsWalkResult,
 ):
     songs = service.gather_songs(raw_artist_with_one_album_with_one_song)
-    assert songs._songs[0].artist.name == "test artist"
-    assert songs._songs[0].album.name == "test album"
-    assert songs._songs[0].title == "First song"
-    assert len(songs._songs) == 1
-    assert not songs.is_empty()
+    assert songs[0].artist_name == "test artist"
+    assert songs[0].album_name == "test album"
+    assert songs[0].title == "First song"
+    assert len(songs) == 1
